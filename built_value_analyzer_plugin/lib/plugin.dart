388dcb93812f2a6bfd4032ac6db4a90ee3c5c140
David Morgan <davidmorgan@google.com>
2017-11-03 16:47:11 +0100

    Trying.

--- built_value_analyzer_plugin/lib/plugin.dart
+++ built_value_analyzer_plugin/lib/plugin.dart
@@ -2,7 +2,7 @@ import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/src/dart/analysis/driver.dart';
 import 'package:analyzer_plugin/plugin/plugin.dart';
 import 'package:analyzer_plugin/protocol/protocol_generated.dart';
-import 'package:built_value_generator_analyzer_plugin/driver.dart';
+import 'package:built_value_analyzer_plugin/driver.dart';
 
 class BuiltValueAnalyzerPlugin extends ServerPlugin {
   BuiltValueAnalyzerPlugin(ResourceProvider provider) : super(provider);

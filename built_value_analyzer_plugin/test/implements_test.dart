090b0dbd6f985c33d33c682e9f4009925d9310d5
David Morgan <davidmorgan@google.com>
2018-02-14 18:08:42 -0500

    First proto test.

--- built_value_analyzer_plugin/test/implements_test.dart
+++ built_value_analyzer_plugin/test/implements_test.dart
@@ -7,12 +7,25 @@ void main() {
 
   group('corrects implements statement', () {
     test('with no generics', () async {
+      final src = 'class Foo implements Built {}';
+      final totalSrc = 'library test_library; class Built {}; $src';
+
       final element = await resolveSource(
-                  'library test_library; class Built {}; class Foo implements Built {}',
-                  (resolver) => resolver.findLibraryByName('test_library'));
+          totalSrc, (resolver) => resolver.findLibraryByName('test_library'));
 
       final results = checker.check(element);
-      throw results;
+
+      var fixedSrc = src;
+      for (final correction in results.values) {
+        for (final edits in correction.change.edits) {
+          for (final edit in edits.edits) {
+            fixedSrc = fixedSrc.replaceRange(
+                edit.offset, edit.length, edit.replacement);
+          }
+        }
+      }
+
+      expect(fixedSrc, 'class Foo implements Built<Foo, Bar> {}');
     });
   });
 }

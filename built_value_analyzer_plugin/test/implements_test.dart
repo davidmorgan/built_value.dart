422554a1776ccdbc12d1f552fb4844dea5f8d778
David Morgan <davidmorgan@google.com>
2018-02-14 18:03:19 -0500

    First proto test.

--- built_value_analyzer_plugin/test/implements_test.dart
+++ built_value_analyzer_plugin/test/implements_test.dart
@@ -1,7 +1,18 @@
+import 'package:build_test/build_test.dart';
+import 'package:built_value_analyzer_plugin/checker.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('foo', () {
-    throw 'foo';
+  final checker = new Checker();
+
+  group('corrects implements statement', () {
+    test('with no generics', () async {
+      final element = await resolveSource(
+                  'library test_library; class Built {}; class Foo implements Built {}',
+                  (resolver) => resolver.findLibraryByName('test_library'));
+
+      final results = checker.check(element);
+      throw results;
+    });
   });
 }

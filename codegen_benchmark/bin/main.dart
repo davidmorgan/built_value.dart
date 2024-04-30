becedbba93e4e9a1fe4f7eac57a7eeca93419dde
David Morgan <davidmorgan@google.com>
2024-04-30 17:13:27 +0200

    Runner.

--- codegen_benchmark/bin/main.dart
+++ codegen_benchmark/bin/main.dart
@@ -6,33 +6,51 @@ Future<void> main() async {
   final runner = CodegenBenchmarkRunner();
   await runner.run();
 
-  for (var i = 0; i != 1000; ++i) {
-    runner.workspace.write('lib/foo$i.dart', source: createClass(i));
-  }
+  for (var max = 1; max < 1000; max *= 2) {
+    for (var i = 0; i != max; ++i) {
+      runner.workspace.write('lib/foo$i.dart', source: createClass(i, max));
+    }
 
-  for (var i = 0; i != 30; ++i) {
+    var future = runner.analysisServer
+        .timeWaitForMessageMs(hasDiagnosticMessage('unused_import'));
     runner.workspace.write('lib/foo0.dart',
-        source: createClass(0, withError: true, withCacheBuster: true));
-    print(await runner.analysisServer
-        .timeWaitForMessageMs(hasDiagnosticMessage('unused_import')));
+        source: createClass(0, max, withError: true, withCacheBuster: true));
+    await future;
 
+    future =
+        runner.analysisServer.timeWaitForMessageMs(hasNoDiagnosticMessages());
     runner.workspace.write('lib/foo0.dart',
-        source: createClass(0, withError: false, withCacheBuster: true));
-    print(await runner.analysisServer
-        .timeWaitForMessageMs(hasNoDiagnosticMessages()));
+        source: createClass(0, max, withError: false, withCacheBuster: true));
+    await future;
+
+    final results = [];
+    for (var i = 0; i != 15; ++i) {
+      future = runner.analysisServer
+          .timeWaitForMessageMs(hasDiagnosticMessage('unused_import'));
+      runner.workspace.write('lib/foo0.dart',
+          source: createClass(0, max, withError: true, withCacheBuster: true));
+      results.add(await future);
+
+      future =
+          runner.analysisServer.timeWaitForMessageMs(hasNoDiagnosticMessages());
+      runner.workspace.write('lib/foo0.dart',
+          source: createClass(0, max, withError: false, withCacheBuster: true));
+      results.add(await future);
+    }
+    print('$max,${results.join(',')}');
   }
 }
 
-String createClass(int i,
+String createClass(int i, int max,
     {bool withError = false, bool withCacheBuster = false}) {
-  final nextI = (i + 1) % 1000;
+  final nextI = (i + 1) % max;
   final blob = StringBuffer();
   for (var i = 0; i != 1000; ++i) {
-    blob.write('int? foo$i;');
+    blob.write('int? foo$i;\n');
   }
   return '''
 ${withError ? 'import "dart:io";' : ''}
-import 'foo${nextI}.dart';
+${i != nextI ? "import 'foo${nextI}.dart';" : ''}
 class Foo$i {
   Foo${nextI}? next;
   ${blob}
